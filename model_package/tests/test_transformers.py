13756e2344776f30973d58c5568ba8c0f9272a94
kdrivas <k.rivas@pucp.edu.pe>
2024-06-08 16:10:37 -0500

    Add tests for utils

---
+++ model_package/tests/test_transformers.py
@@ -0,0 +1,48 @@
+"""
+    This file contains transformer classes for scikit learn pipelines.
+    The intention is to reuse the code
+"""
+import pytest
+import pandas as pd
+from model.utils.transformers import (
+    DropCols
+)
+
+
+@pytest.mark.parametrize(
+    "input, drop_cols, expected_output",
+    [
+        pytest.param(
+            {
+                "a": [1, 2, 3],
+                "b": [4, 5, 6],
+                "c": [7, 8, 9],
+            },
+            ["a", "b"],
+            [
+                {"c": 7}, {"c":  8}, {"c": 9},
+            ],
+            id="drop_0",
+        ),
+        pytest.param(
+            {
+                "a": [1, 2, 3],
+                "c": [7, 8, 9],
+            },
+            ["a"],
+            [
+                {"c": 7}, {"c":  8}, {"c": 9},
+            ],
+            id="drop_1",
+        )
+    ]
+)
+def test_drop_cols_transformer(
+    input: dict, drop_cols: list, expected_output: dict
+):
+    df_input = pd.DataFrame(input)
+    drop = DropCols(cols=drop_cols)
+
+    df_output = drop.fit_transform(df_input)
+
+    assert df_output.to_dict("records") == expected_output
